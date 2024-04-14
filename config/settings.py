6461d594d850853ee0595029b223356e26c2d301
Edward Mike <greenpython.developer@gmail.com>
2024-04-15 00:28:39 +0100

    config. postgres database

--- config/settings.py
+++ config/settings.py
@@ -153,25 +153,18 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
+DATABASES = {
 
-#     'default': {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT', cast=int)
+    }
 
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-#         'NAME': 'dev_blog_db',
-
-#         'USER': 'postgres',
-
-#         'PASSWORD': 'admin123',
-
-#         'HOST': 'localhost',
-
-#         'PORT': 5432,
-
-#     }
-
-# }
+}
 
 # CACHES
 # ------------------------------------------------------------------------------
